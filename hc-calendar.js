3ec21bd244a4532d53078dab9ccdf866dc0b0661
nagmo01 <bjworkshop01@gmail.com>
2024-02-16 16:56:06 +0900

    動作成功

--- hc-calendar.js
+++ hc-calendar.js
@@ -23,21 +23,23 @@ const numbers = Array.from({length: 12}, (_, i) => (i + 1).toString());
 function checkOption(optionMonth){
   if(optionMonth === undefined){
     //未指定なので今月のカレンダーを表示させる
-    return new Date();
+    let date = new Date();
+    date.setDate(1);
+    return date;
 
   } else if(numbers.includes(optionMonth)){
 
     //1~12月で指定されたので指定された月のカレンダーを表示させる
     //こちらの変数にオプション引数の値が1の場合も01に直して文字列取得で使える形にして返す。
     let stringMonth
-    let today = new Date();
+    let someday = new Date();
 
     if (optionMonth.length === 1){
       stringMonth = "0"+ optionMonth;
     } else {
       stringMonth = optionMonth;
     }
-    return new Date(String(today.getFullYear())+"-"+stringMonth+"-"+"01");
+    return new Date(String(someday.getFullYear())+"-"+stringMonth+"-"+"01");
 
   } else {
     console.log("不正な月が指定されたのでエラーを表示させる")
@@ -49,8 +51,8 @@ function checkOption(optionMonth){
 console.log("表示するカレンダーの日付");
 //dateに入力された月、または当月どちらかの1日のデータを入れる
 let date = checkOption(optionMonth);
-console.log(date);
-
+console.log(date.getMonth());
+//このときdateにはoptionがあればその月の1日、なければ今月の1日が入る。
 
 
 
@@ -58,7 +60,7 @@ console.log(date);
 const weekArray = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"];
 
 
-const getDay = date.getDate;
+const getDay = date.getDay();
 
 //let date = checkOption((optionMonth));
 //console.log(date);
